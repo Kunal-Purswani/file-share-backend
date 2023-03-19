a8590aa57770a2389b60b6d1bff020b570a2589a
Kunal Purswani <kunalpurswani40@gmail.com>
2023-03-19 22:24:41 +0530

    second commit

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ import upload from "../utils/upload.js";
 
 const router = express.Router();
 
+router.get('/',res.send('Home Page'))
 router.post('/upload',upload.single('file'),uploadImage);
 router.get('/file/:fileId',downloadImage);
 export default router;
